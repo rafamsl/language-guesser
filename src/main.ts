90a3564e3910b10712a8eba5b825bb5bc49c6a34
rafamsl <rafael.msl81@gmail.com>
2023-07-06 12:08:29 +0200

    public

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
 
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  app.useStaticAssets(join(__dirname,'..','public'));
 
   await app.listen(8080);
 }
